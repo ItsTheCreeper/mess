2f47664c7191940c868d82e39f35e0360b9b0380
ItsTheCreeper <38690339+ItsTheCreeper@users.noreply.github.com>
2019-05-29 21:45:40 +0800

    Hide overflow @ original

--- scripts/mess-original/main.js
+++ scripts/mess-original/main.js
@@ -2,6 +2,8 @@ var canvas = document.getElementById("myCanvas");
 canvas.width = document.body.clientWidth;
 canvas.height = window.screen.availHeight;
 
+document.body.style = "overflow: hidden; margin: 0; ";
+
 var myCircle;
 
 var Circle = function () {
