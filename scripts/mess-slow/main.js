0907826439c57e7afb6a62bbb75be3fa6b4309b1
ItsTheCreeper <38690339+ItsTheCreeper@users.noreply.github.com>
2019-06-01 19:34:56 +0800

    Create and Add main-slow

---
+++ scripts/mess-slow/main.js
@@ -0,0 +1,24 @@
+var canvas = document.getElementById("myCanvas");
+canvas.width = document.body.clientWidth;
+canvas.height = window.screen.availHeight;
+
+document.body.style = "overflow: hidden; margin: 0;";
+
+var myCircle;
+
+var Circle = function () {
+    this.x = Math.floor(Math.random() * canvas.width);
+    this.y = Math.floor(Math.random() * canvas.height);
+    this.radius = (Math.floor(Math.random() * 300) + 25); // change number for maximum/minimum radius
+    this.draw = function () {
+        var ctx = canvas.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+        ctx.stroke();
+    };
+};
+
+setInterval(function() {
+    myCircle = new Circle();
+    myCircle.draw();
+}, 1000);
